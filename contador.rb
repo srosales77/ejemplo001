8e512cd5dd82d7808c22b4f503f27efc36d2c3f5
Sergio Rosales <sergio.ra@cdguzman.tecnm.mx>
2024-09-01 04:05:54 +0000

    crea contador de palabras

---
+++ contador.rb
@@ -0,0 +1,27 @@
+class AnalizaTexto
+    
+    def initialize(texto)
+        @texto = texto
+    end
+
+    def cadena_palabras
+        @texto.downcase.scan(/[\w]+/)
+    end
+
+    def cuenta
+        lista_de_palabras = cadena_palabras
+        cuenta = Hash.new(0)
+        for palabra in lista_de_palabras
+            cuenta[palabra] += 1
+        end
+        cuenta
+    end
+end
+
+analisis = AnalizaTexto.new("Este texto es un texto como este")
+
+
+puts analisis.cuenta
+
+
+analisis.cuenta.each {|key,value| puts  " palabra #{key}, veces #{value}" }
