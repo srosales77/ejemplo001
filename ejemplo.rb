aeee0dcce70e8efdbf3d61a28e64a031ca05c6cf
Sergio Rosales <sergio.ra@cdguzman.tecnm.mx>
2024-09-01 02:49:45 +0000

    modifica ejemplos

--- ejemplo.rb
+++ ejemplo.rb
@@ -1,7 +1,9 @@
 class Padre
+    def initialize(nombre)
+        @nombre = nombre
+    end
     def say_hello
-        saludo = "Hola desde #{self}"
-        return saludo
+        "Hola #{@nombre} desde  #{self}"
     end
 end
 
@@ -9,10 +11,10 @@ class Hijo < Padre
 end
 
 
-saluda1 = Padre.new
+saluda1 = Padre.new("Darth")
 puts saluda1.say_hello
 
-saluda2 = Hijo.new
+saluda2 = Hijo.new("Anakin")
 puts saluda2.say_hello
 
 
